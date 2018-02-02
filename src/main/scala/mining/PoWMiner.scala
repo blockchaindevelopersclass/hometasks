50c577bf7381ffff4946d1b3ab7a2a183e50c94c
catena <catena@protonmail.com>
2018-02-02 19:14:24 +0300

    Signature forger task

--- src/main/scala/mining/PoWMiner.scala
+++ src/main/scala/mining/PoWMiner.scala
@@ -8,8 +8,14 @@ class PoWMiner[HF <: CryptographicHash32](hashFunction: HF) {
 
   private val MaxTarget: BigInt = BigInt(1, Array.fill(32)((-1).toByte))
 
-  def doWork(data: Array[Byte], difficulty: BigInt): ProvedData = ???
-
+  def doWork(data: Array[Byte], difficulty: BigInt): ProvedData = {
+    def loop(nonce: Int):ProvedData = {
+      val d = ProvedData(data, nonce)
+      if(validateWork(d, difficulty)) d
+      else loop(nonce + 1)
+    }
+    loop(0)
+  }
   def validateWork(data: ProvedData, difficulty: BigInt): Boolean = realDifficulty(data) >= difficulty
 
   private def realDifficulty(noncedData: ProvedData): BigInt =
