6ec814b390e2e51e85ec8b673f24c5ad2b9d73de
catena <catena@protonmail.com>
2018-02-20 13:28:13 +0300

    BDMempool

---
+++ src/test/scala/transaction/BDMempoolTest.scala
@@ -0,0 +1,20 @@
+package transaction
+
+import org.scalacheck.Gen
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
+import org.scalatest.{Matchers, PropSpec}
+import scorex.testkit.generators.CoreGenerators
+import scorex.testkit.properties.mempool.MempoolTransactionsTest
+
+class BDMempoolTest extends PropSpec
+  with PropertyChecks
+  with GeneratorDrivenPropertyChecks
+  with Matchers
+  with CoreGenerators
+  with MempoolTransactionsTest[Sha256PreimageProposition, BlockchainDevelopersTransaction, BlockchainDevelopersMempool]
+{
+
+  override val memPool: BlockchainDevelopersMempool = _
+  override val transactionGenerator: Gen[BlockchainDevelopersTransaction] = _
+
+}
